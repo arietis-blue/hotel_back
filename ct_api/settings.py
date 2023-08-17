0658d0a44ff4b597ace843115a1d448d89c3af7f
arietis-blue <arietis20324@gmail.com>
2023-08-18 07:54:34 +0900

    cors

--- ct_api/settings.py
+++ ct_api/settings.py
@@ -60,9 +60,14 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'https://restaurantfront-a94054c84e78.herokuapp.com',
 ]
 
 CORS_ORIGIN_WHITELIST = ( 
